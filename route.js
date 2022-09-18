8145ace7049783db94bed2c0cb530dd3389be25e
Jayson Gelera <61036844+jhajhay@users.noreply.github.com>
2022-09-18 19:22:07 +0800

    Update route.js

--- route.js
+++ route.js
@@ -1,9 +1,18 @@
 const route = {};
-
-route['/'] = ['get','users','index'];
-route['/logoff'] = ['get','users','logoff'];
-route['/login'] = ['post','users','login'];
-route['/register'] = ['post','users','register'];
-route['/students/profile'] = ['get','users','profile'];
+/*
+*  Example routes: 
+*  Without get data
+*  route['/'] = ['get','users','index'];
+*
+*  With get data
+*  route['/:id'] = ['get','users','index'];
+*
+*  Route post method
+*  route['/register'] = ['post','users','register'];
+*  
+*  Route with middleware
+*  route['/register'] = ['post',[middleware1,middleware2.....],'users','register'];
+*  route['/'] = ['get',[middleware1,middleware2.....],'users','index'];
+*/
 
 module.exports = route;
